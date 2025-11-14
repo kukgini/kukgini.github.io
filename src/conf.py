928b22cd877a683e9962020301fa47219baeca2d
kukgini <kukgini@gmail.com>
2025-11-14 13:16:53 +0900

    Apply sphinx-book-theme for better documentation UI

--- src/conf.py
+++ src/conf.py
@@ -88,14 +88,20 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+html_theme_options = {
+    "repository_url": "https://github.com/kukgini/kukgini.github.io",
+    "use_repository_button": True,
+    "use_edit_page_button": False,
+    "use_issues_button": False,
+    "use_download_button": True,
+    "home_page_in_toc": True,
+    "show_navbar_depth": 2,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
